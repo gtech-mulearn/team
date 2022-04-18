268b32bde10e4bd9f4e7a5d58149e5ff9c4ea5fa
Aswin Asok <aswinasokofficial@gmail.com>
2022-04-18 22:32:17 +0530

    Content Changes

--- script.js
+++
@@ -1,7 +0,0 @@
-let ipaddress;
-$.getJSON("https://api64.ipify.org?format=json", function (data) {
-  ipaddress = String(data.ip);
-  var a = document.getElementById("url"); //or grab it by tagname etc
-  var uniqueid = btoa(`${ipaddress}-${new Date().toString()}`);
-  a.href = `https://airtable.com/shr1B63pF5WggZzLf?prefill_Unique+ID=${uniqueid}`;
-});
